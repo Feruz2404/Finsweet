460c89368b943a1ed260287f08b311d6ebd48a4f
Feruz2404 <rferuz96@gmail.com>
2024-12-05 16:40:24 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Service from "./pages/service/Service";
+import Company from "./pages/company/Company";
+import Career from "./pages/career/Career";
+import Home from "./pages/home/Home";
+import Layout from "./pages/layout/Layout";
+import LogIn from "./pages/login/LogIn";
+import NotFound from "./pages/NotFound/NotFound.jsx";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/career" element={<Career />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
